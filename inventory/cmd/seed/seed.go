a1a25d0b8ef67389fed7bf7bd71b097840fa8d4c
Felipe Sere <felipesere@gmail.com>
2020-04-12 17:24:13 +0200

    Use config to configure seed cmd

--- inventory/cmd/seed/seed.go
+++ inventory/cmd/seed/seed.go
@@ -4,14 +4,34 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/BoRuDar/configuration"
 	"github.com/felipesere/inventory/v0/pkg/products"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
+
+	cfg := struct {
+		Host string `flag:"host" default:"localhost"`
+		Port int64 	`flag:"port" default:"8080"`
+		File string `flag:"file"`
+	}{}
+
+	conf, err := configuration.New(&cfg, []configuration.Provider{
+		configuration.NewFlagProvider(&cfg),
+		configuration.NewDefaultProvider(),
+	},
+		false,
+		true,
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+	conf.InitValues()
+
 	fmt.Println("Reading seeds...")
-	content, err := ioutil.ReadFile("./seed.json")
+	content, err := ioutil.ReadFile(cfg.File)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -29,7 +49,7 @@ func main() {
 	for _, item := range seeds.Items {
 		fmt.Println(fmt.Sprintf("Seeding item %s", item.Name))
 		itemAsJSON, _ := json.Marshal(item)
-		_, err := http.Post("http://localhost:8080/inventory/available", "application/json", bytes.NewReader(itemAsJSON))
+		_, err := http.Post(fmt.Sprintf("http://%s:%d/inventory/available", cfg.Host, cfg.Port), "application/json", bytes.NewReader(itemAsJSON))
 		if err != nil {
 			panic(err.Error())
 		}
